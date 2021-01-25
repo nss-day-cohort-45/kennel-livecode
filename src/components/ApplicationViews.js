d1fcf54c1f85c00aa54a7b8eecdb3662258548ee
Joe Shepherd <joeshepmedia@gmail.com>
2021-01-24 19:44:32 -0600

    list animals -- animal provider/list/card

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
-import { AnimalCard } from "./animal/AnimalCard"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
 
 export const ApplicationViews = () => {
     return (
@@ -12,9 +13,11 @@ export const ApplicationViews = () => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
+            <AnimalProvider>
+              <Route path="/animals">
+                <AnimalList />
+              </Route>
+            </AnimalProvider>
         </>
     )
 }
