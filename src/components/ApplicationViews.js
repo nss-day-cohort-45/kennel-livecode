9e2071b83df312ab193d570fd47862b674ed9117
Joe Shepherd <joeshepmedia@gmail.com>
2021-01-27 08:55:16 -0600

    Animal detail view

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalProvider } from "./animal/AnimalProvider"
 import { AnimalList } from "./animal/AnimalList"
@@ -8,13 +8,14 @@ import { LocationList } from "./location/LocationList"
 import { CustomerProvider } from "./customer/CustomerProvider"
 import { CustomerList } from "./customer/CustomerList"
 import { AnimalForm } from "./animal/AnimalForm"
+import { AnimalDetail} from "./animal/AnimalDetail"
 
 export const ApplicationViews = () => {
     return (
         <>
             {/* Render the location list when http://localhost:3000/ */}
             <Route exact path="/">
-                <Home />
+              <Home />
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
@@ -30,6 +31,12 @@ export const ApplicationViews = () => {
                   </Route>
                 </CustomerProvider>
               </LocationProvider>
+            </AnimalProvider>
+
+            <AnimalProvider>
+              <Route exact path="/animals/detail/:animalId(\d+)">
+                  <AnimalDetail />
+              </Route>
             </AnimalProvider>
 
             <LocationProvider>
--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,18 +1,16 @@
 import React from "react"
 import "./Animal.css"
+import { Link } from "react-router-dom"
 
-export const AnimalCard = (props) => {
-    // this..
-    // const animal = props.animal
-    // const owner = props.owner
-    // const location = props.location
-    // is the same as...
-    const { animal } = props
-    return (<section className="animal fred">
-        <h3 className="animal__name">{animal.name}</h3>
-        <div className="animal__breed">Breed: {animal.breed}</div>
-        <div className="animal__owner">Owner: {animal.customer.name}</div>
-        <div className="animal__location">{animal.location.name}</div>
 
-    </section>
+export const AnimalCard = ({ animal }) => {
+  return (
+    <section className="animal">
+      <h3 className="animal__name">
+        <Link to={`/animals/detail/${animal.id}`}>
+          { animal.name }
+        </Link>
+      </h3>
+      <div className="animal__breed">{ animal.breed }</div>
+  </section>
 )}
